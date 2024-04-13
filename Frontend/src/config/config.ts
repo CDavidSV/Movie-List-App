16e4ee71966765c418cc8e076b7327218b377918
CDavidSV <88672259+CDavidSV@users.noreply.github.com>
2024-04-12 18:36:16 -0600

    Update config.ts

--- Frontend/src/config/config.ts
+++ Frontend/src/config/config.ts
@@ -1,5 +1,5 @@
 const config = {
-    apiURL: import.meta.env.MODE === 'production' ? import.meta.env.VITE_API_URL_PROD : 'http://localhost:3000',
+    apiURL: import.meta.env.MODE === 'production' ? import.meta.env.VITE_API_URL_PROD : 'http://localhost:8080',
 };
 
 export default config;
