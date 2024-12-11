fcb84679dd024bdaf9d4e7cd99ac881c88646d7b
CDavidSV <cdavidsv6@gmail.com>
2024-12-10 18:28:15 -0600

    Drag and drop now works on mobile

--- Frontend/src/pages/Favorites/Favorites.tsx
+++ Frontend/src/pages/Favorites/Favorites.tsx
@@ -6,9 +6,9 @@ import useInfiniteScroll from "../../hooks/useInfiniteScroll";
 import { GlobalContext } from "../../contexts/GlobalContext";
 import { ToastContext } from "../../contexts/ToastContext";
 import { GripVertical, Trash2 } from "lucide-react";
-import { closestCorners, DndContext, DragEndEvent } from "@dnd-kit/core";
+import { closestCorners, DndContext, DragEndEvent, KeyboardSensor, PointerSensor, TouchSensor, useSensor, useSensors } from "@dnd-kit/core";
 import { CSS } from "@dnd-kit/utilities";
-import { SortableContext, useSortable, verticalListSortingStrategy } from "@dnd-kit/sortable";
+import { SortableContext, sortableKeyboardCoordinates, useSortable, verticalListSortingStrategy } from "@dnd-kit/sortable";
 import "./favorites.css";
 
 function FilmListCard({ filmData, removeItem }: { filmData: any, removeItem: Function }) {
@@ -84,6 +84,14 @@ export default function Favorites() {
 
     useInfiniteScroll(() => getNextPage(), loading, !lastRank);
 
+    const sensors = useSensors(
+        useSensor(PointerSensor),
+        useSensor(TouchSensor),
+        useSensor(KeyboardSensor, {
+            coordinateGetter: sortableKeyboardCoordinates,
+        })
+    );
+
     useEffect(() => {
         document.title = "Favorites | My Movie List";
 
@@ -127,8 +135,7 @@ export default function Favorites() {
         const activeIndex = favorites.findIndex((item) => item.favorite_id === active.id);
         const overIndex = favorites.findIndex((item) => item.favorite_id === over.id);
 
-        const updatedFavorites = [...favorites];
-
+        const original = [...favorites];
         // Call the api to reorder the items
         mml_api_protected.post("api/v1/favorites/reorder", {
             ref_id: favorites[overIndex].favorite_id,
@@ -136,10 +143,11 @@ export default function Favorites() {
             position: overIndex > activeIndex ? "after" : "before"
         }).catch(() => {
             toast.open("Error reordering favorites", "error");
-            setFavorites(updatedFavorites);
+            setFavorites(original);
         });
 
         // Move the reordered items
+        const updatedFavorites = [...favorites];
         const [temp] = updatedFavorites.splice(activeIndex, 1);
         updatedFavorites.splice(overIndex, 0, temp);
 
@@ -161,7 +169,7 @@ export default function Favorites() {
                         </Link>
                     </div>
                     :
-                    <DndContext collisionDetection={closestCorners} onDragEnd={handleOnDragEnd}>
+                    <DndContext collisionDetection={closestCorners} onDragEnd={handleOnDragEnd} sensors={sensors}>
                         <SortableContext
                             items={favorites.map((i) => i.favorite_id)}
                             strategy={verticalListSortingStrategy}
